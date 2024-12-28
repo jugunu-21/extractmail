fc89335a9d981289df958bb029cd6331cbf1912b
jugunu-21 <aaruhijugunu@gmail.com>
2024-12-28 17:38:22 +0530

    add content

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,64 @@
 "use client"
+import { EmailInput } from "@/components/email-input";
+import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
 import { Vortex } from "@/components/ui/vortex";
-import Image from "next/image";
-
 export default function Home() {
   return (
     <>
-      <div className="   border-2 ">
-        <div className="w-[calc(100%-4rem)] mx-auto rounded-md  h-[50rem] overflow-hidden">
-          <Vortex
-            backgroundColor="black"
-            className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
-          >
-            <h2 className="text-white text-2xl md:text-6xl font-bold text-center">
-              The hell is this?
-            </h2>
-            <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
-              This is chemical burn. It&apos;ll hurt more than you&apos;ve ever been
-              burned and you&apos;ll have a scar.
-            </p>
-            <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
-              <button className="px-4 py-2 bg-blue-600 hover:bg-blue-700 transition duration-200 rounded-lg text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset]">
-                Order now
-              </button>
-              <button className="px-4 py-2  text-white ">Watch trailer</button>
+      <div className="w-[calc(100%-4rem)] mx-auto rounded-md  h-full overflow-scroll  bg-black ">
+        <Vortex
+          backgroundColor="black"
+          className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full  h-full "
+        >
+          <h2 className="text-white text-2xl md:text-6xl font-bold text-center pt-6">
+            OutsmartAI is coming...
+          </h2>
+          <p className="text-white text-sm md:text-xl max-w-xl mt-6 text-center pb-2">
+            And when it arrives, nothing will be the same.
+            You&apos;re about to witness something huge.
+            But we&apos;re not giving away the details just yet.
+            What we can tell you is this:
+          </p>
+          <div className="p-2 rounded-full">
+            <div className="flex items-center justify-center bg-gray-50 px-6 py-6 text-neutral-800 rounded-sm">
+
+              <div className="w-full max-w-xl space-y-2">
+                <TextGenerateEffect words="This is the future of performance marketing" />
+                <p className=" ">   And if you're not in, you're going to regret it.</p>
+                <hr className="border-neutral-300" />
+                <p className="">The clock&apos;s ticking.
+
+                </p>
+                <p>
+                  Early access will be limited, and trust us
+                  <strong> —you do not want to be the one who misses this.</strong>
+
+                </p>
+                <hr className="border-neutral-300" />
+                <p> You&apos;ve seen the hype.<br />
+                  You&apos;ve heard the whispers.<br />
+                  But when the curtain pulls back, only the smartest brands will be ahead of the curve.</p>
+                <strong>Are you one of them?</strong>
+                <hr className="border-neutral-300" />
+                <p>    Sign up.</p>
+                <strong> Before it&apos;s too late.</strong>
+                <hr className="border-neutral-300" />
+                <div>
+                  <p>This is your chance to be the first to experience something that&apos;s about to shake up the industry.</p>
+                  <hr className="border-neutral-300" />
+                </div>
+                <div className="py-2"><strong> [Sign up for exclusive early access]</strong></div>
+                <EmailInput />
+              </div>
             </div>
-          </Vortex>
-        </div>
+
+          </div>
+        </Vortex>
+
       </div>
+
 
     </>
 
   );
-}
+}   

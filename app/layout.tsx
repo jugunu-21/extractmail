6334616f8cc073216a44cfaed02bc0824ecfa586
jugunu-21 <aaruhijugunu@gmail.com>
2024-12-29 16:28:41 +0530

    implement google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export const metadata: Metadata = {
   description: "",
 };
 
+// ... rest of your imports
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -25,6 +26,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+            function gtag() {dataLayer.push(arguments); }
+        gtag('js', new Date());
+        gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS},{
+        page_path:windows.loctaion.pathname,
+        });`,
+          }}
+
+        />
+      </head>
+
       <body
       // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- components/functions.ts
+++ components/functions.ts
@@ -2,18 +2,19 @@
 import { User } from '@prisma/client';
 import prismadb from "@/db";
 import { error } from 'console';
-export async function createUser(email: string): Promise<User | null> {
-    console.log("email", email)
-    try {
 
+
+export async function createUser(email: string): Promise<User | null | Error> {
+    console.log("email", email);
+    try {
         const user = await prismadb.user.create({
             data: {
                 email,
             },
-        })
-        return user
+        });
+
+        return user;
     } catch (error) {
         throw error;
-
     }
 }

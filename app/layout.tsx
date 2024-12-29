2141a943003070a94c130bbda4227d730068e818
jugunu-21 <aaruhijugunu@gmail.com>
2024-12-29 17:15:25 +0530

    remove the script error

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,8 @@ export default function RootLayout({
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+
         `,
           }}
         />
